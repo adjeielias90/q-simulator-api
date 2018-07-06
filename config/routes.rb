9cffdd4c8a2190f8176376f7d2273303dbcbc6fe
Adjei Elias <adjeielias90@gmail.com>
2018-07-06 10:07:37 +0100

    Updated app routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Questionnaire::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :subjects
-  resources :mockexams
+  resources :mockexams do
+    resources :mock_questions do
+      resources :mock_answers do
+        resources :mock_correct_answers
+      end
+    end
+  end
   resources :surveys
   root to: 'surveys#index'
 end
